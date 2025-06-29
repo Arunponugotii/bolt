f814c81e837f6b7a7da3b808425f78033b4dc446
Arunponugotii <97236440+Arunponugotii@users.noreply.github.com>
2025-06-29 20:54:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,7 @@ resource "google_container_cluster" "primary" {
 
   # Specify node locations (zones) for the cluster
   node_locations = local.node_zones
+  deletion_protection = false 
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
