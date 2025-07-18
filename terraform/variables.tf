48fefcae96076d2a51ddfbf39817753a2b135542
Arunponugotii <97236440+Arunponugotii@users.noreply.github.com>
2025-07-18 07:31:32 +0530

    Update terraform/variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,35 +6,27 @@ variable "project_id" {
 variable "cluster_name" {
   description = "The name of the GKE cluster"
   type        = string
-  default     = "my-gke-cluster"
 }
 
 variable "region" {
   description = "The GCP region for the cluster"
   type        = string
-  default     = "us-central1"
-}
-
-variable "node_count" {
-  description = "Number of nodes in the node pool"
-  type        = number
-  default     = 2
 }
 
 variable "machine_type" {
-  description = "Machine type for the nodes"
+  description = "The machine type for cluster nodes"
   type        = string
   default     = "e2-medium"
 }
 
-variable "disk_size" {
-  description = "Disk size in GB for each node"
+variable "node_count" {
+  description = "The number of nodes in the cluster"
   type        = number
-  default     = 100
+  default     = 2
 }
 
-variable "service_account_email" {
-  description = "Service account email for GKE cluster and nodes"
-  type        = string
-  default     = "githubactions-sa@turnkey-guild-441104-f3.iam.gserviceaccount.com"
+variable "disk_size" {
+  description = "The disk size for cluster nodes in GB"
+  type        = number
+  default     = 20
 }
