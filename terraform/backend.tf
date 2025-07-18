3df474ce479b185d0f7070d61edc757d8bff78bc
Arunponugotii <97236440+Arunponugotii@users.noreply.github.com>
2025-07-18 07:31:29 +0530

    Update terraform/backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,4 +3,13 @@ terraform {
     bucket = "terraform-statefile-bucket-tf2"
     prefix = "terraform/state/gke-cluster"
   }
+  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+  
+  required_version = ">= 1.0"
 }
